0b3b55d669ad65f1c7f2bae5071db766e0606463
NavyaRaja <110919834+NavyaRaja@users.noreply.github.com>
2022-08-21 13:43:54 +0530

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -3,34 +3,35 @@
 
 # # 1.Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
 
-# In[1]:
-
-
 break = 5
 print ('break')
 
+output:
+  Input In [1]
+    break = 5
+          ^
+SyntaxError: invalid syntax
 
 # # 2.Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
-
-# In[9]:
-
 
 Birth_Year=1997
 Current_Year=2022
 Age=Current_Year-Birth_Year
 Age
 
+output:
+  25
+
 
 # # 3.Store your first, middle and last name in three different variables and then print your full name using these variables
-
-# In[14]:
-
 
 Frist_Name = 'Rajana'
 Second_Name = 'Navya'
 Full_Name= Frist_Name + '  ' + Second_Name
 Full_Name
 
+output:
+  'Rajana  Navya'
 
 # # 4.Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
 
